1ed33be4ea2b1a7a4ce1d086f6e6f5f603ae5400
ayesha-firdaus <ayeshafirdaus21@gmail.com>
2024-03-13 14:06:56 +0530

    create user model

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,12 @@
 const express=require("express");
+const mongoose=require("mongoose");
+const dotenv=require("dotenv");
+dotenv.config({});
+mongoose.connect(process.env.mongo).then((con)=>{
+    console.log("sucessfully connected to database")
+}).catch(err=>{
+    console.log(err);
+})
 const app=express();
 app.listen(3000,()=>{
     console.log("server is running on port 3000");
